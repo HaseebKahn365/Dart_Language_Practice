9318df3eac8318a88cf9256b84132d79e171f90f
HaseebKahn365 <58126830+HaseebKahn365@users.noreply.github.com>
2023-07-26 23:16:05 +0500

    Update _9OperatorsOnCollection.dart

--- Mastering_Dart/Collections/_9OperatorsOnCollection.dart
+++ Mastering_Dart/Collections/_9OperatorsOnCollection.dart
@@ -9,4 +9,15 @@ void main() {
   //
   final List<int> twices = list.map((e) => e * 2).toList();
   print(twices);
+  final Squares = transform(list, (x) => x * x);
+  print(Squares);
+}
+
+List<int> transform(List<int> items, int Function(int) f) {
+  //the function can be passed as an argument
+  var result = <int>[];
+  for (var x in items) {
+    result.add(f(x));
+  }
+  return result;
 }
